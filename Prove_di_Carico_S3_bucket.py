dc18851b1ec77862ca37c408c42fe9f3cb05dab9
Francesca Del Corso <francesca.delcorso@pg.infn.it>
2024-12-04 10:31:52 +0100

    Update Prove_di_Carico_S3_bucket.py

--- Prove_di_Carico_S3_bucket.py
+++ Prove_di_Carico_S3_bucket.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
 
 
-bucket_list=['repo01','repo02','repo03','repo04','repo05','repo06','repo07','repo08','repo09','repo10','repo11','repo12','repo13','repo14','repo15','repo16','repo17','repo18','repo19','repo20']  #bucket_list=['repo01','repo02','repo03','repo04','repo05','repo06','repo07','repo08','repo09','repo10']
+bucket_list=['repo01','repo02','repo03','repo04','repo05','repo06','repo07','repo08','repo09','repo10','repo11','repo12','repo13','repo14','repo15','repo16','repo17','repo18','repo19','repo20']  
 path = "Prove_di_carico\\sw\\small\\100\\"
 output_file = "Prove_di_carico\\Prove_di_Carico.txt"
 
@@ -28,20 +28,16 @@ def get_oidc_token():
 def get_s3_client(TOKEN_IAM):
         if TOKEN_IAM:
                 print(f"Token IAM updated: {TOKEN_IAM}")
-                # sts=Secure Token Service initialization
                 sts_client = boto3.client('sts',
                                 endpoint_url="https://rgw.cloud.infn.it:443",
                                 region_name='default'
                                 )
-                # Perform assume_role_with_web_identity
                 response = sts_client.assume_role_with_web_identity(
                                 RoleArn="arn:aws:iam:::role/IAMaccess",
                                 RoleSessionName='Bob',
                                 DurationSeconds=3600,
                                 WebIdentityToken = TOKEN_IAM
                                 )
-
-                # After AuthN and AuthZ success, use temporary credentials stored in response variable to proceed with s3 operations
                 s3_client = boto3.client('s3',
                                 aws_access_key_id = response['Credentials']['AccessKeyId'],
                                 aws_secret_access_key = response['Credentials']['SecretAccessKey'],
@@ -49,7 +45,6 @@ def get_s3_client(TOKEN_IAM):
                                 endpoint_url="https://rgw.cloud.infn.it:443",
                                 region_name='default',
                                 )
-                #print(response["Credentials"])
         else:
                 print("Failed to update token.")
 
@@ -79,7 +74,6 @@ def list_bucket_content(bucket):
         return(bucket_objects)
 
 while True:
- # Sarebbe il main()
  try:
         # For Debugging Purposes 
         #boto3.set_stream_logger(name='botocore')
